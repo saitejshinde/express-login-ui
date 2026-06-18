4c3e5caf7ff4769d1e24d79ec23d7c8cc702669e
Saitej Shinde <saitej.g.shinde@gmail.com>
2026-06-18 20:38:18 +0530

    commit 2

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post('/login',(req,res)=>{
     const {user, passwd}= req.body;
     if(user === passwd)
         res.send({
-            volume:"Hello From Saitej",
+            
             status: "success",
             message: "Login successful"
         });
